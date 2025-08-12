d9c967ee13768fd167d54e3978a45f5c913068d1
ChargeCHKNMAN <83246927+ChargeCHKNMAN@users.noreply.github.com>
2025-08-12 14:08:47 +1000

    Update app.py

--- Grange Yadh/app.py
+++ Grange Yadh/app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 # Timetable with 6 time slots and class numbers
 student_timetables = {
     "SAY0023": [
-        ["Math (C101)", "English (C102)", "Science (C103)", "History (C104)", "PE (C105)", "Art (C106)"],
-        ["Biology (C107)", "Chemistry (C108)", "Math (C101)", "Art (C106)", "Music (C109)", "Drama (C110)"],
-        ["English (C102)", "Math (C101)", "Geography (C111)", "PE (C105)", "Drama (C110)", "Science (C103)"],
-        ["History (C104)", "Science (C103)", "Math (C101)", "Music (C109)", "Art (C106)", "English (C102)"],
-        ["Math (C101)", "PE (C105)", "Science (C103)", "English (C102)", "Art (C106)", "Music (C109)"]
+        ["English (C3)", "English (C3)", "IT (B4)", "IT (B4)", "Business (C9)", "Business (C9)"],
+        ["Business (C9)", "Physics (F4)", "Methods (C10)", "Methods (C10)", "IT (B4)", "English (C3)"],
+        ["General Maths (C9)", "General Maths (C9)", "Physics (F4)", "Physics (F4)", "Methods (C10)", "Methods (C10)"],
+        ["General Maths (C9)", "Methods (C10)", "IT (B4)", "IT (B4)", "English (C3)", "English (C3)"],
+        ["Business (C9)", "Business (C9)", "Physics (F4)", "Physics (F4)", "General Maths (C9)", "General Maths (C9)"]
     ],
     "BINH0031": [
         ["Science (C103)", "Math (C101)", "English (C102)", "Art (C106)", "Music (C109)", "Drama (C110)"],
@@ -75,3 +75,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
